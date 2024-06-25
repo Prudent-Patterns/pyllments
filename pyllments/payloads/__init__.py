a0d6900c36d54cee097901482cb18ad3a238b05a
DmitriyL <dmleybel@gmail.com>
2024-06-25 05:47:21 +0000

    daily checkpoint

--- pyllments/payloads/__init__.py
+++ pyllments/payloads/__init__.py
@@ -1,1 +1,2 @@
-# Other payload types: Text, Embedding, Documenet, Query, Response, Context, Feedback, Metrics
+# TODO: Other payload types: Text, Embedding, Documenet, Query, Response, Context, Feedback, Metrics
+# TODO: Consider creatingg payload handlers
--- pyllments/ports/ports.py
+++ pyllments/ports/ports.py
@@ -1,16 +1,23 @@
 import param
 
-class OutputPorts(param.Parameterized):
-    output_map = param.Dict(default={})
+from pyllments.base import Payload
 
-    def add_output(self, parameter):
+class OutputPort(param.Parameterized):
+    output_map = param.Dict(default=dict())
+    payload_type = param.ClassSelector(class_=Payload)
+
+    def add_output(self, parameter, ):
         new_param = param.Parameter(default=None, allow_refs=True, per_instance=True)
         self.param.add_parameter(parameter.name, new_param)
         self.param.update({parameter.name: parameter})
         self.output_map[parameter.name] = []
+    
+    def emit(self, ):
+        pass
+    def stage(self, ):
+        pass
 
-
-class InputPorts(param.Parameterized):
+class InputPort(param.Parameterized):
     inputs = param.List(item_type=tuple[str, str]) 
 
     def add_input(self, parameter, element=None):
