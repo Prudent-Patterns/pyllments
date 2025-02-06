11617bbc3249a00eb2207c3e66105c620f38099b
DmitriyL <dmleybel@gmail.com>
2025-02-06 03:08:48 +0000

    Added reacttivity in embedder element and embedder support class

--- pyllments/elements/embedder/embedder_model.py
+++ pyllments/elements/embedder/embedder_model.py
@@ -3,26 +3,28 @@ import param
 from pyllments.base.model_base import Model
 from pyllments.payloads.chunk.chunk_payload import ChunkPayload
 from pyllments.payloads.message.message_payload import MessagePayload
-from .encoders import SentenceTransformerEncoder
+from .embedders import SentenceTransformerEmbedder
 
 
 class EmbedderModel(Model):
-    encoder_model = param.Parameter(default=None)
-    encoder_model_class = param.Parameter(default=SentenceTransformerEncoder, doc="""
+    embedder_model = param.Parameter(default=None)
+    embedder_model_class = param.Parameter(default=SentenceTransformerEmbedder, doc="""
         Should return a list of embeddings given a list of sentences""")
-    encoder_model_name = param.String(default="Alibaba-NLP/gte-base-en-v1.5")
+    embedder_model_name = param.String(default="Alibaba-NLP/gte-base-en-v1.5")
     embedding_dims = param.Integer(default=768, doc="The dimension of the embedding")
 
     def __init__(self, **params):
         super().__init__(**params)
-        self.encoder_model = self.encoder_model_class(model_name=self.encoder_model_name)
-        self.embedding_dims = self.encoder_model.embedding_dims
+        self.embedder_model = self.embedder_model_class(model_name=self.embedder_model_name)
+        self.embedding_dims = self.embedder_model.embedding_dims
+
+        self.param.watch(self._on_embedder_model_name_change, 'embedder_model_name')
 
     def embed_chunks(self, chunks: list[ChunkPayload]) -> list[ChunkPayload]:
         """
         Embed a list of chunks and return the processed chunks with embeddings.
         """
-        embed_list = self.encoder_model.encode([chunk.model.text for chunk in chunks])
+        embed_list = self.embedder_model.encode([chunk.model.text for chunk in chunks])
         for chunk, embedding in zip(chunks, embed_list):
             chunk.model.embedding = embedding
         return chunks
@@ -39,7 +41,7 @@ class EmbedderModel(Model):
         Embed a list of messages and return the processed messages with embeddings.
         """
         
-        embed_list = self.encoder_model.encode([message.model.message.content for message in messages])
+        embed_list = self.embedder_model.encode([message.model.message.content for message in messages])
         for message, embedding in zip(messages, embed_list):
             message.model.embedding = embedding
         return messages
@@ -50,3 +52,7 @@ class EmbedderModel(Model):
         """
         message = self.embed_messages([message])[0]
         return message
+    
+    def _on_embedder_model_name_change(self, event):
+        self.embedder_model.model_name = self.embedder_model_name
+        self.embedding_dims = self.embedder_model.embedding_dims
--- pyllments/elements/embedder/embedders.py
+++ pyllments/elements/embedder/embedders.py
@@ -1,15 +1,16 @@
 from numpy import ndarray
 import param
+from sentence_transformers import SentenceTransformer
 
 
-class SentenceTransformerEncoder(param.Parameterized):
+class SentenceTransformerEmbedder(param.Parameterized):
     model_name = param.String(default="Alibaba-NLP/gte-base-en-v1.5", doc="""
         The name of the model to use""")
     
     model = param.Parameter(default=None)
     embedding_dims = param.Integer(default=768, doc="""
         The dimension of the embedding""")
-    cache_folder = param.String(default=None)
+    cache_folder = param.String(default=None, doc="'~/.cache' if not specified") 
 
     def __init__(self, **params):
         super().__init__(**params)
@@ -18,9 +19,19 @@ class SentenceTransformerEncoder(param.Parameterized):
         self.model = SentenceTransformer(
             self.model_name,
             trust_remote_code=True,
-            cache_folder=self.cache_folder
+            cache_folder=self.cache_folder 
         )
         self.embedding_dims = self.model.get_sentence_embedding_dimension()
 
-    def encode(self, sentences: list[str]) -> ndarray:
+        self.param.watch(self._on_model_change, 'model_name')
+
+    def embed(self, sentences: list[str]) -> ndarray:
         return self.model.encode(sentences)
+    
+    def _on_model_change(self, event):
+        self.model = SentenceTransformer(
+            self.model_name,
+            trust_remote_code=True,
+            cache_folder=self.cache_folder 
+        )
+        self.embedding_dims = self.model.get_sentence_embedding_dimension()
